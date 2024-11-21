3337592388f9d1aaca1b628d04fb74f9869a4328
Altaf Parves <altafparvesh@gmail.com>
2024-11-21 18:05:18 +0700

    feat: responsive navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../logo";
 import styles from "./styles.module.css";
 import CartIcon from "../../assets/icons/ic-cart.svg";
 import SearchIcon from "../../assets/icons/ic-search.svg";
 import HeartIcon from "../../assets/icons/ic-heart.svg";
 import AccIcon from "../../assets/icons/ic-person.svg";
-import { useState } from "react";
+
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen]=useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
+
   return (
-    <nav className="w-full px-[35px] bg-[#fff] py-[10px] flex flex-row justify-between items-center">
-      <Logo></Logo>
+    <nav className={styles.navbar}>
+      <Logo />
       <div
-        className={`absolute left-0 top-[60px] w-full bg-white py-4 flex flex-col items-center gap-4 transition-all duration-500 ease-in-out ${isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-[20px] pointer-events-none"}`}
+        className={`${
+          isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-[20px] pointer-events-none"
+        } absolute lg:static left-0 top-[60px] lg:flex w-full lg:w-auto bg-white lg:bg-transparent py-4 lg:py-0 flex-col lg:flex-row lg:items-center gap-4 lg:gap-[50px] transition-all duration-500 ease-in-out lg:gap-[70px] xl:gap-[125px]`}
       >
-        <div className={styles.menuLeft}>
+        {/* Menu Left */}
+        <div className={`${styles.menuLeft} lg:flex-row`}>
           <button className={styles.menuItem}>Home</button>
           <button className={styles.menuItem}>Shop</button>
           <button className={styles.menuItem}>About</button>
           <button className={styles.menuItem}>Contact</button>
           <button className={styles.menuItem}>Pages</button>
         </div>
-        <div className={styles.menuRight}>
+        {/* Menu Right */}
+        <div className={`${styles.menuRight} lg:flex-row`}>
           <button className={styles.actionBtn}>
-            <img src={AccIcon} alt="" />
-            <p className="text-[30px]"> Login/Register</p>
+            <img src={AccIcon} className={styles.IcBtn} alt="Account" />
+            <p className="font-[400] text-[30px] lg:text-[14px]">Login / Register</p>
           </button>
           <button className={styles.actionBtn}>
-            <img src={SearchIcon} alt="" />
+            <img className={styles.IcBtn} src={SearchIcon} alt="Search" />
           </button>
           <button className={styles.actionBtn}>
-            <img src={CartIcon} alt="" />1
+            <img className={styles.IcBtn} src={CartIcon} alt="Cart" />1
           </button>
           <button className={styles.actionBtn}>
-            <img src={HeartIcon} alt="" />1
+            <img className={styles.IcBtn} src={HeartIcon} alt="Favorites" />1
           </button>
         </div>
       </div>
-      <button type="button" className="burger" onClick={toggleMenu}>
+      <button type="button" className="burger block lg:hidden" onClick={toggleMenu}>
         <svg width="24" height="14" viewBox="0 0 24 14" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0.571472 0H23.4286V2.28571H0.571472V0ZM6.28576 5.71429H23.4286V8H6.28576V5.71429ZM13.4286 11.4286H23.4286V13.7143H13.4286V11.4286Z" fill="#737373" />
         </svg>
@@ -48,3 +54,4 @@ export default function Navbar() {
     </nav>
   );
 }
+
--- src/layout/mainLayout.jsx
+++ src/layout/mainLayout.jsx
@@ -6,8 +6,8 @@ export default function MainLayout({ children }) {
   return (
     <>
       <div className="w-screen h-scree bg-[#FAFAFA] p-0">
-        <Navbar />
-        <p className="breadCrumb flex flex-row justify-center items-center w-full py-[34px] lg:justify-start lg:px-[100px] text-black font-bold text-sm gap-[15px]">
+        <Navbar/>
+        <p className="mt-[82px] breadCrumb flex flex-row justify-center items-center w-full py-[34px] lg:justify-start lg:px-[100px] text-black font-bold text-sm gap-[15px]">
           Home
           <img src={ArrowIc} alt="" />
           <span className="text-light_gray"> Shop</span>
